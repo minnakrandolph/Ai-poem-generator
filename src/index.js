3db6e25c9f5f4cb3b10bb7beee4ff2b62a86e1fd
minna <minnakrandolph@gmail.com>
2024-08-17 19:39:51 -0400

    Basic JS behavior
    added Typrwriter.js plug-in to the index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-//alert("hello world");
+function generatePoem(event) {
+    event.preventDefault();
+
+    let poemElement = document.querySelector("#poem");
+    
+    new Typewriter(`#poem`, {
+    strings: [`Lord, 
+        when you send the rain
+        think about it, please,
+        a little?
+        <br />
+Do
+        not get carried away
+        by the sound of falling water,
+        the marvelous light
+        on the falling water.
+        <br />
+    I
+        am beneath that water.
+        It falls with great force
+        and the light
+        <br />
+Blinds
+        me to the light.
+        <br />
+
+        (Untitled by James Bladwin) `],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+});
+    
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
