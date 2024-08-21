5584ff18f9096033c08c74980b91b96aa73fc243
minna <minnakrandolph@gmail.com>
2024-08-21 08:30:27 -0400

    updated files
    added axios, added API generating keys, and integration. Established prompts, and context.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
+// display generated poem
+function displayPoem(response) {
+new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+        
+    }
+
 function generatePoem(event) {
     event.preventDefault();
-
+// build the Api URL
     let poemElement = document.querySelector("#poem");
-    
-    new Typewriter(`#poem`, {
-    strings: [`Lord, 
-        when you send the rain
-        think about it, please,
-        a little?
-        <br />
-Do
-        not get carried away
-        by the sound of falling water,
-        the marvelous light
-        on the falling water.
-        <br />
-    I
-        am beneath that water.
-        It falls with great force
-        and the light
-        <br />
-Blinds
-        me to the light.
-        <br />
 
-        (Untitled by James Bladwin) `],
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-});
-    
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
+    let prompt = `Generate a poem about ${instructionsInput.value}`;
+    let context = "You are an expert in rythmic contemporary style prose. Please make sure to follow user instructions. Peace, and thank you! :)";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+   
+    console.log("Generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Prompt: ${context}`);
+   
+    // Make a call to the APi using axios
+    axios.get(apiUrl).then(displayPoem);
 }
+
+   
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
