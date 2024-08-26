ea9e2f5985e37f6dcf64a6859f67aac143dea3c9
minna <minnakrandolph@gmail.com>
2024-08-26 07:55:42 -0400

    Modified UX
    Adding a temp message with animation

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ new Typewriter("#poem", {
 function generatePoem(event) {
     event.preventDefault();
 // build the Api URL
-    let poemElement = document.querySelector("#poem");
+    
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "2e3dto1e48d1a435aab54b3f664a20b0";
     let prompt = `Generate a poem about ${instructionsInput.value}`;
-    let context = "You are an expert in rythmic contemporary style prose. Please make sure to follow user instructions. Peace, and thank you! :)";
+    let context = "You are an expert in gritty rythmic contemporary style prose with sarcastic humor and drama. Please make sure to follow user instructions. Peace, and thank you! :)";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
    
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating"> ⏳Here is a poem about ${instructionsInput.value}! </div>`;
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Prompt: ${context}`);
